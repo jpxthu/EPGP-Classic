737db63ca348adb9785aab13ba7667cd3531243b
jpx <jinpx@qq.com>
2020-09-15 23:49:24 +0800

    v1.5.3
    Bug fix

--- modules/reminder.lua
+++ modules/reminder.lua
@@ -2,6 +2,8 @@ local mod = EPGP:NewModule("reminder", "AceEvent-3.0")
 local DLG = LibStub("LibDialog-1.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("EPGP")
 
+local in_raid = false
+
 local function EnableCombatlog()
   if not EPGP.db.profile.remind_enable_combatlog then
     return
@@ -10,17 +12,26 @@ local function EnableCombatlog()
     EPGP:Print(L["COMBATLOG_IS_LOGGING"])
     return
   end
-  if UnitInRaid("player") then
+  if in_raid then
     DLG:Spawn("EPGP_REMIND_ENABLE_COMBATLOG")
   end
 end
 
 local function Init()
+  in_raid = UnitInRaid("player")
+  EnableCombatlog()
+end
+
+local function RaidtatusChanged()
   EnableCombatlog()
 end
 
 function mod:GROUP_ROSTER_UPDATE()
-  EnableCombatlog()
+  local tmp = UnitInRaid("player")
+  if in_raid ~= tmp then
+    in_raid = tmp
+    RaidtatusChanged()
+  end
 end
 
 function mod:OnEnable()
